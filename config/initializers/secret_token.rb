b2dba319107a0bc643fe069b6ff7a1d62aa54b6d
Bo Zhang <bozhang@watercooler-inc.com>
2013-07-16 17:04:13 +0800

    init rails env

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularRailsDemo::Application.config.secret_key_base = 'ee8c11ba47429a9490ec024bb30b0e79f113b3bac47c19a148bc04cc681ca2dd8a15f33aeb7ab6af0467292052b587c20470566bfe5098901aa3fdebfd2eee79'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AngularRailsDemo::Application.config.session_store :cookie_store, key: '_angular_rails_demo_session'
